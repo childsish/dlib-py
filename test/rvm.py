6c0b5df96ba72f5b72b1f163d045b13a123b7b7e
Liam Childs <liam_childs@hotmail.com>
2014-02-20 17:35:03 +0100

    Made tests unit tests

--- test/rvm.py
+++ test/rvm.py
@@ -1,96 +1,139 @@
-'''
-Created on 28.03.2013
-
-@author: Liam
-'''
-
 import math
+import itertools
+import unittest
+import tempfile
 
-from dlib import _rvm
+from dlib._rvm import compute_mean_squared_distance as msd
 from dlib.rvm import *
-from unittest import TestCase
 
-class TestVectorNormaliser(TestCase):
-    def test_train(self):
-        pass
+class TestRVMs(unittest.TestCase):
+    def setUp(self):
+        self.c_samples = [(float(r), float(c)) for r, c in\
+            itertools.product(xrange(-20, 20), xrange(-20, 20))]
+        self.c_labels = [1 if math.sqrt(r*r + c*c) <= 10 else -1\
+            for r, c in self.c_samples]
+        
+        self.r_samples = [(x,) for x in range(-10, 4)]
+        self.r_labels = [sinc(x) for (x,) in self.r_samples]
+        
+        self.fhndl, self.fname = tempfile.mkstemp()
+
+    def test_rvc(self):
+        labels = self.c_labels
+        normalizer = VectorNormalizer()
+        normalizer.train(self.c_samples)
+        samples = [normalizer(sample) for sample in self.c_samples]
+        trainer = Trainer(RadialBasisKernel(0.08), 0.01)
+        fn = NormalizedFunction(trainer.train(samples, labels), normalizer)
+
+        self.assertGreaterEqual(fn((3.123, 2)), 0)
+        self.assertGreaterEqual(fn((3.123, 9.3545)), 0)
+        self.assertLess(fn((13.123, 9.3545)), 0)
+        self.assertLess(fn((13.123, 0)), 0)
+        
+    def test_probabilistic_rvc(self):
+        labels = self.c_labels
+        normalizer = VectorNormalizer()
+        normalizer.train(self.c_samples)
+        samples = [normalizer(sample) for sample in self.c_samples]
+        trainer = Trainer(RadialBasisKernel(0.08), 0.01)
+        pfn = NormalizedFunction(trainer.trainProbabilistic(samples, labels),
+            normalizer)
+        
+        self.assertGreaterEqual(pfn((3.123, 2)), 0.5)
+        self.assertGreaterEqual(pfn((3.123, 9.3545)), 0.5)
+        self.assertLess(pfn((13.123, 9.3545)), 0.5)
+        self.assertLess(pfn((13.123, 0)), 0.5)
+    
+    def test_serialize_rvc(self):
+        labels = self.c_labels
+        normalizer = VectorNormalizer()
+        normalizer.train(self.c_samples)
+        samples = [normalizer(sample) for sample in self.c_samples]
+        trainer = Trainer(RadialBasisKernel(0.08), 0.01)
+        fn = NormalizedFunction(trainer.train(samples, labels), normalizer)
+        fn.serialize(self.fname)
+        fn = NormalizedFunction.deserialize(self.fname)
+
+        self.assertGreaterEqual(fn((3.123, 2)), 0)
+        self.assertGreaterEqual(fn((3.123, 9.3545)), 0)
+        self.assertLess(fn((13.123, 9.3545)), 0)
+        self.assertLess(fn((13.123, 0)), 0)
+        
+    def test_serialize_probabilistic_rvc(self):
+        labels = self.c_labels
+        normalizer = VectorNormalizer()
+        normalizer.train(self.c_samples)
+        samples = [normalizer(sample) for sample in self.c_samples]
+        trainer = Trainer(RadialBasisKernel(0.08), 0.01)
+        fn = NormalizedFunction(trainer.trainProbabilistic(samples, labels),
+            normalizer)
+        fn.serialize(self.fname)
+        fn = NormalizedFunction.deserialize(self.fname)
+
+        self.assertGreaterEqual(fn((3.123, 2)), 0.5)
+        self.assertGreaterEqual(fn((3.123, 9.3545)), 0.5)
+        self.assertLess(fn((13.123, 9.3545)), 0.5)
+        self.assertLess(fn((13.123, 0)), 0.5)
+    
+    def test_rvr(self):
+        gamma = 2.0 / msd(VectorSample(self.r_samples))
+        trainer = RegressionTrainer(RadialBasisKernel(gamma), 0.00001)
+        fn = trainer.train(self.r_samples, self.r_labels)
+        
+        self.assertAlmostEqual(fn((2.5,)), 0.2594000)
+        self.assertAlmostEqual(fn((0.1,)), 0.9812903)
+        self.assertAlmostEqual(fn((-4.,)), -0.1188682)
+        self.assertAlmostEqual(fn((5.0,)), -0.69175494)
+    
+    def test_serialize_rvr(self):
+        gamma = 2.0 / msd(VectorSample(self.r_samples))
+        trainer = RegressionTrainer(RadialBasisKernel(gamma), 0.00001)
+        fn = trainer.train(self.r_samples, self.r_labels)
+        fn.serialize(self.fname)
+        fn = DecisionFunction.deserialize(self.fname)
+        
+        self.assertAlmostEqual(fn((2.5,)), 0.2594000)
+        self.assertAlmostEqual(fn((0.1,)), 0.9812903)
+        self.assertAlmostEqual(fn((-4.,)), -0.1188682)
+        self.assertAlmostEqual(fn((5.0,)), -0.69175494)
+    
+    def test_basisVectors(self):
+        labels = self.c_labels
+        normalizer = VectorNormalizer()
+        normalizer.train(self.c_samples)
+        samples = [normalizer(sample) for sample in self.c_samples]
+        trainer = Trainer(RadialBasisKernel(0.08), 0.01)
+        fn = NormalizedFunction(trainer.train(samples, labels), normalizer)
+        vectors = fn.basis_vectors
+    
+        self.assertEquals(len(vectors), 10)
+        self.assertAlmostEquals(vectors[0][0], -1.6887495)
+        self.assertAlmostEquals(vectors[0][1], -1.6887495)
+        self.assertAlmostEquals(vectors[1][0], -1.6887495)
+        self.assertAlmostEquals(vectors[1][1], 1.6887495)
+        self.assertAlmostEquals(vectors[2][0], -0.8227241)
+        self.assertAlmostEquals(vectors[2][1], -1.2557368)
+        self.assertAlmostEquals(vectors[3][0], -0.4763140)
+        self.assertAlmostEquals(vectors[3][1], -0.5629165)
+        self.assertAlmostEquals(vectors[4][0], -0.4763140)
+        self.assertAlmostEquals(vectors[4][1], 0.3031089)
+        self.assertAlmostEquals(vectors[5][0], 0.1299038)
+        self.assertAlmostEquals(vectors[5][1], 0.1299038)
+        self.assertAlmostEquals(vectors[6][0], 0.3897114)
+        self.assertAlmostEquals(vectors[6][1], -0.0433013)
+        self.assertAlmostEquals(vectors[7][0], 1.2557368)
+        self.assertAlmostEquals(vectors[7][1], 0.7361216)
+        self.assertAlmostEquals(vectors[8][0], 1.5155445)
+        self.assertAlmostEquals(vectors[8][1], 1.5155445)
+        self.assertAlmostEquals(vectors[9][0], 1.6887495)
+        self.assertAlmostEquals(vectors[9][1], -1.6887495)
 
 def sinc(x):
     if x == 0:
         return 1
     return math.sin(x) / x
 
-def test_rvc():
-    samples = []
-    labels = []
-    
-    for r in xrange(-20, 20):
-        for c in xrange(-20, 20):
-            s = (float(r), float(c))
-            samples.append(s)
-            
-            if (math.sqrt(r*r + c*c) <= 10):
-                labels.append(1.)
-            else:
-                labels.append(-1.)
-    
-    normalizer = VectorNormalizer()
-    normalizer.train(samples)
-    for i in xrange(len(samples)):
-        samples[i] = normalizer(samples[i])
-    
-    trainer = Trainer(RadialBasisKernel(0.08), 0.01)
-    
-    fn = NormalizedFunction(trainer.train(samples, labels), normalizer)
-    
-    for x, y in ((3.123, 2), (3.123, 9.3545)):
-        print "This sample should be >= 0 and it is classified as a %.3f"%fn((x, y))
-
-    for x, y in ((13.123, 9.3545), (13.123, 0)):
-        print "This sample should be < 0 and it is classified as a %.3f"%fn((x, y))
-    
-    fn.serialize("saved_function.dat")
-    fn = NormalizedFunction.deserialize("saved_function.dat")
-    print 'Testing deserialized function: %s'%fn((x, y))
-    
-    pfn = NormalizedFunction(trainer.trainProbabilistic(samples, labels), normalizer)
-    
-    for x, y in ((3.123, 2), (3.123, 9.3545)):
-        print "This +1 example should have high probability. Its probability is: %.3f"%pfn((x, y))
-    
-    for x, y in ((13.123, 9.3545), (13.123, 0)):
-        print "This -1 example should have low probability. Its probability is: %.3f"%pfn((x, y))
-    
-    pfn.serialize("saved_function.dat")
-    pfn = NormalizedFunction.deserialize("saved_function.dat")
-    print 'Testing deserialized function: %s'%pfn(s)
-
-def test_rvr():
-    samples = []
-    labels = []
-    
-    for x in xrange(-10, 4):
-        samples.append((x,))
-        labels.append(sinc(x))
-    
-    gamma = 2.0 / _rvm.compute_mean_squared_distance(VectorSample(samples))
-    trainer = RegressionTrainer(RadialBasisKernel(gamma), 0.00001)
-    print 'using gamma of', gamma
-    
-    fn = trainer.train(samples, labels)
-    
-    m = (2.5,); print sinc(m[0]), fn(m)
-    m = (0.1,); print sinc(m[0]), fn(m)
-    m = (-4.,); print sinc(m[0]), fn(m)
-    m = (5.0,); print sinc(m[0]), fn(m)
-    
-    fn.serialize("saved_function.dat")
-    fn = DecisionFunction.deserialize("saved_function.dat")
-    print 'Testing deserialized funtcion: %s'%fn(m)
-
-def main():
-    test_rvc()
-    test_rvr()
-
 if __name__ == '__main__':
     import sys
-    sys.exit(main())
+    sys.exit(unittest.main())
